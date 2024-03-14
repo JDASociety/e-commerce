7142507ef589ca12bb4ded5dd419886eaa6d0471
diegosiac <diego35502@hotmail.com>
2024-03-14 14:59:35 -0600

    Add Next UI provider

---
+++ src/providers/next-ui-provider.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from 'react'
+import { NextUIProvider } from '@nextui-org/react'
+
+interface Props {
+	children: ReactNode
+}
+
+export const NextUiProvider = ({ children }: Props) => {
+	return <NextUIProvider>{children}</NextUIProvider>
+}
