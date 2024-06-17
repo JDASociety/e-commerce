eb2ea258e9ab01aa05f64599296c02ec306e0e83
diegosiac <diego35502@hotmail.com>
2024-06-17 07:29:10 -0600

    feat: Add clothes size variables

---
+++ src/product/vars/clothes-size.ts
@@ -0,0 +1,34 @@
+import type { ClothesSize } from '../interfaces'
+
+export const clothesSize: ClothesSize[] = [
+	{
+		id: '1',
+		name: 'XS',
+		description: 'Pequeña',
+	},
+	{
+		id: '2',
+		name: 'S',
+		description: 'Mediana',
+	},
+	{
+		id: '3',
+		name: 'M',
+		description: 'Larga',
+	},
+	{
+		id: '4',
+		name: 'L',
+		description: 'Grande',
+	},
+	{
+		id: '5',
+		name: 'XL',
+		description: 'Extra Grande',
+	},
+	{
+		id: '6',
+		name: 'XXL',
+		description: 'Extra Extra Grande',
+	},
+]
