6cbe2497fc6e56688418b29e6616316d8c29b3bc
diegosiac <diego35502@hotmail.com>
2024-06-17 07:26:26 -0600

    chore: Update eslint and tailwindcss dependencies, and fix eslint configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,41 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'png.pngtree.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'ae01.alicdn.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'ih1.redbubble.net'
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.shutterstock.com'
+      },
+    {
+        protocol: 'https',
+        hostname: 'i.etsystatic.com'
+      }
+    ]
+  }
+}
 
-export default nextConfig;
+export default nextConfig
