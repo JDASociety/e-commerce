dad5f30310e33473eddd333bffbf4b33add626c8
diegosiac <diego35502@hotmail.com>
2024-06-25 06:16:41 -0600

    feat: Update ProductDetailsPage to fetch and display product details dynamically

--- src/app/(sg)/product/[slug]/page.tsx
+++ src/app/(sg)/product/[slug]/page.tsx
@@ -1,20 +1,47 @@
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
 import {
 	ButtonLikeProduct,
 	PreviewProductImages,
+	ProductInformationAccordion,
 	ProductListCarousel,
 } from '@/product/components'
+import { formatCurrencyNumber, getProductDetails } from '@/product/helpers'
 import { buttonVariants, PosterPromotion } from '@/ui/components'
-import { ArrowLeftIcon, CheckIcon, StarFilledIcon } from '@/ui/icons'
+import { ArrowLeftIcon, CheckIcon } from '@/ui/icons'
+import { RatingStars } from '@/reviews/components'
 import { FormAddToCartShopping } from './ui/form-add-to-cart-shopping'
 import { products } from '@/mock'
 
-export default function ProductDetailsPage({
-	...props
+export default async function ProductDetailsPage({
+	params,
 }: {
-	params: { slug?: string }
+	params: { slug: string }
 }) {
-	console.log(props)
+	const product = await getProductDetails(params.slug)
+
+	if (!product.ok) {
+		redirect('/')
+	}
+
+	const {
+		brand,
+		clothingCut,
+		color,
+		description,
+		discount,
+		id,
+		images,
+		isLiked,
+		name,
+		price,
+		rating,
+		slug,
+		stock,
+		totalReviews,
+		details,
+		reviews,
+	} = product.product
 
 	return (
 		<section className="px-10 py-6">
@@ -29,82 +56,88 @@ export default function ProductDetailsPage({
 					Volver al Inicio
 				</Link>
 			</div>
-			<div className="flex flex-col md:flex-row gap-10">
+			<div className="grid grid-cols-1 md:grid-cols-2 gap-10">
 				<PreviewProductImages />
-				<div>
-					<div className="flex justify-between">
-						<h1 className="font-semibold text-balance text-2xl">
-							Camisa de Algodón
-						</h1>
-						<ButtonLikeProduct productId="" isLiked={false} />
-					</div>
-					<p className="max-w-[65ch] h-[72px] overflow-hidden text-ellipsis">
-						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae
-						excepturi numquam maxime vero suscipit! Suscipit consectetur
-						reprehenderit explicabo laborum iusto? Dolore hic explicabo, quod
-						iusto magni ipsum beatae molestias iure? adf añskdf ñasldkf ñalskd
-						fjñalsdk fjñ asdfñlasdf lkasd faklsdjf
-						<div>
-							<span>
-								<span></span>
-							</span>
-							<span>Envío en 5-7 días</span>
+				<div className="md:row-span-2 relative">
+					<div className="sticky top-0 left-0">
+						<div className="flex justify-between">
+							<h1 className="font-semibold text-balance text-2xl">{name}</h1>
+							<ButtonLikeProduct productId={id} isLiked={!!isLiked} />
 						</div>
-					</p>
-					<div className="mt-4">
-						<span className="text-3xl font-semibold">$95</span>
-						<span className="block text-sm">IVA incluido</span>
-					</div>
-					<Link
-						href="#"
-						className={buttonVariants({
-							variant: 'link',
-							className: 'gap-1 items-center pl-0',
-						})}>
-						<StarFilledIcon className="size-5" />
-						<StarFilledIcon className="size-5" />
-						<StarFilledIcon className="size-5" />
-						<StarFilledIcon className="size-5" />
-						<StarFilledIcon className="size-5" />
-						<span className="ml-2">(12)</span>
-					</Link>
-					<FormAddToCartShopping />
-					<div className="flex justify-between text-sm mt-4">
-						<span className="font-medium flex gap-2 items-center">
-							<span className="relative flex size-3">
-								<span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-primary opacity-40"></span>
-								<span className="relative inline-flex rounded-full size-3 bg-primary/35"></span>
-							</span>
-							Disponible
-						</span>
-						<span className="opacity-70">Envío en 5-7 días hábiles</span>
-					</div>
-					<div className="mt-6">
-						<p className="max-w-[65ch]">
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
-							corporis perspiciatis culpa commodi quae laboriosam esse magni
+						<p className="max-w-[65ch] h-[72px] overflow-hidden text-ellipsis">
+							{description}
 						</p>
-						<ul className="mt-5">
-							<li className="mt-2 flex gap-3 items-center">
-								<CheckIcon className="size-5 stroke-2" />
-								Lorem ipsum dolor sit amet.
-							</li>
-							<li className="mt-2 flex gap-3 items-center">
-								<CheckIcon className="size-5 stroke-2" />
-								magni corrupti quibusdam nemo.
-							</li>
-							<li className="mt-2 flex gap-3 items-center">
-								<CheckIcon className="size-5 stroke-2" />
-								consectetur adipisicing elit.
-							</li>
+						<div className="mt-4">
+							<div className="flex gap-2 items-end">
+								<span className="text-3xl font-semibold">
+									{formatCurrencyNumber(price)}
+								</span>
 
-							<li className="mt-2 flex gap-3 items-center">
-								<CheckIcon className="size-5 stroke-2" />
-								rem qui enim architecto harum provident
-							</li>
-						</ul>
+								{discount && (
+									<>
+										<span className="opacity-85 text line-through">
+											{formatCurrencyNumber(price - (price * discount) / 100)}
+										</span>
+										<span className="font-bold">-{discount}% OFF</span>
+									</>
+								)}
+							</div>
+							<span className="text-sm">IVA incluido</span>
+						</div>
+						<Link
+							href="#product-reviews"
+							className={buttonVariants({
+								variant: 'link',
+								className: 'gap-1 items-center pl-0',
+							})}>
+							<RatingStars rating={rating} />
+							<span className="ml-2">({totalReviews})</span>
+						</Link>
+						<FormAddToCartShopping />
+						<div className="flex justify-between text-sm mt-4">
+							<span className="font-medium flex gap-2 items-center">
+								<span className="relative flex size-3">
+									<span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-primary opacity-40"></span>
+									<span className="relative inline-flex rounded-full size-3 bg-primary/35"></span>
+								</span>
+								Disponible
+							</span>
+							<span className="opacity-70">Envío en 5-7 días hábiles</span>
+						</div>
+						<div className="mt-10">
+							<p className="max-w-[65ch]">
+								Lorem ipsum dolor sit amet consectetur adipisicing elit.
+								Similique corporis perspiciatis culpa commodi quae laboriosam
+								esse magni
+							</p>
+							<ul className="mt-5">
+								<li className="mt-2 flex gap-3 items-center">
+									<CheckIcon className="size-5 stroke-2" />
+									Lorem ipsum dolor sit amet.
+								</li>
+								<li className="mt-2 flex gap-3 items-center">
+									<CheckIcon className="size-5 stroke-2" />
+									magni corrupti quibusdam nemo.
+								</li>
+								<li className="mt-2 flex gap-3 items-center">
+									<CheckIcon className="size-5 stroke-2" />
+									consectetur adipisicing elit.
+								</li>
+
+								<li className="mt-2 flex gap-3 items-center">
+									<CheckIcon className="size-5 stroke-2" />
+									rem qui enim architecto harum provident
+								</li>
+							</ul>
+						</div>
 					</div>
 				</div>
+				<ProductInformationAccordion
+					detailsProduct={details}
+					rating={rating}
+					reviews={reviews}
+					totalReviews={totalReviews}
+				/>
 			</div>
 			<div className="mt-10">
 				<ProductListCarousel
